59e9da517fa4c262c2a1e08a94877dba635b3690
vprasanna <vprasanna@bridge2solutions.com>
2016-05-17 06:58:20 -0400

    Environment Fix

---
+++ scripts/env_config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export OPENSHIFT_MONGODB_DB_HOST=localhost
+export OPENSHIFT_MONGODB_DB_PORT=27017
+
+
+echo "Environment is set"
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+source scripts/env_config.sh
+echo "Shutting down Tomcat"
 ssh 57399fda0c1e66013700000b@shop-venkatvp.rhcloud.com "./app-root/data/apache-tomcat-8.0.33/bin/shutdown.sh"
+echo "Starting Tomcat"
 ssh 57399fda0c1e66013700000b@shop-venkatvp.rhcloud.com "./app-root/data/apache-tomcat-8.0.33/bin/startup.sh"
+echo "Done"
