c7ecdd9c77b1e23842d05245b28cfc386a880178
vprasanna <vprasanna@bridge2solutions.com>
2016-05-18 07:08:28 -0400

    Updated Hook

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 source scripts/env_config.sh
+echo "Clean Tomcat Logs"
+ssh 57399fda0c1e66013700000b@shop-venkatvp.rhcloud.com "rm ./app-root/data/apache-tomcat-8.0.33/logs/*"
 echo "Shutting down Tomcat"
 ssh 57399fda0c1e66013700000b@shop-venkatvp.rhcloud.com "./app-root/data/apache-tomcat-8.0.33/bin/shutdown.sh"
 echo "Starting Tomcat"
