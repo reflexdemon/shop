e3b5a5510554ad7c265629d965dc8f879c1a0b12
reflexdemon <venkateswara.vp@gmail.com>
2019-01-13 14:02:54 -0500

    Organize Imports

--- src/test/java/org/shop/service/AbstractTest.java
+++ src/test/java/org/shop/service/AbstractTest.java
@@ -1,26 +1,12 @@
 package org.shop.service;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
-import org.junit.After;
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.shop.suite.LoadTestData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
 
 /**
  * Created by vprasanna on 5/20/2016.
@@ -29,22 +15,6 @@ import java.io.IOException;
 @SpringBootTest
 @Profile("test")
 public class AbstractTest {
-
-  /**
-   * please store Starter or RuntimeConfig in a static final field
-   * if you want to use artifact store caching (or else disable caching)
-   */
-  private static final MongodStarter starter = MongodStarter.getDefaultInstance();
-
-  private MongodExecutable _mongodExe;
-  private MongodProcess _mongod;
-
-  private MongoClient _mongo;
-  @Autowired
-  private UserServices userServices;
-
-  @Autowired
-  private LoadTestData loadTestData;
 
   @Test
   public void loadContext() {
